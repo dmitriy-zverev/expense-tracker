2a7958659f0878e1fd85ea35ca01ae05d883d0a0
dmitriy-zverev <dimazverev716@gmail.com>
2025-09-08 19:36:56 +0300

    add root cmd command

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,94 @@
 package cmd
+
+import (
+	"errors"
+	"fmt"
+	"slices"
+	"strconv"
+)
+
+type Command struct {
+	ID          int
+	Amount      int
+	Month       int
+	Description string
+	Cmd         string
+}
+
+const (
+	DESCRIPTION_PARAM = "--description"
+	AMOUNT_PARAM      = "--amount"
+	ID_PARAM          = "--id"
+	MONTH_PARAM       = "--month"
+)
+
+func help() {
+	fmt.Printf("Usage: et <command> [-argument 1] [description 1] ...\n")
+	fmt.Printf("	Example: et add --description \"Lunch\" --amount 20\n")
+}
+
+func ParseCommand(args []string) (Command, error) {
+	if len(args) < 2 {
+		help()
+		return Command{}, errors.New("no command found")
+	}
+
+	cmd := Command{
+		Cmd:    args[1],
+		ID:     -1,
+		Month:  -1,
+		Amount: -1,
+	}
+
+	if slices.Contains(args, DESCRIPTION_PARAM) {
+		idx := slices.Index(args, DESCRIPTION_PARAM)
+		if idx+1 > len(args) {
+			return Command{}, errors.New("cannot find argument for --description")
+		}
+		cmd.Description = args[idx+1]
+	}
+
+	if slices.Contains(args, AMOUNT_PARAM) {
+		idx := slices.Index(args, AMOUNT_PARAM)
+		if idx+1 > len(args) {
+			return Command{}, errors.New("cannot find argument for --amount")
+		}
+
+		amount, err := strconv.Atoi(args[idx+1])
+		if err != nil {
+			return Command{}, errors.New("argument for --amount is not a number")
+		}
+
+		cmd.Amount = amount
+	}
+
+	if slices.Contains(args, ID_PARAM) {
+		idx := slices.Index(args, ID_PARAM)
+		if idx+1 > len(args) {
+			return Command{}, errors.New("cannot find argument for --id")
+		}
+
+		id, err := strconv.Atoi(args[idx+1])
+		if err != nil {
+			return Command{}, errors.New("argument for --id is not a number")
+		}
+
+		cmd.ID = id
+	}
+
+	if slices.Contains(args, MONTH_PARAM) {
+		idx := slices.Index(args, MONTH_PARAM)
+		if idx+1 > len(args) {
+			return Command{}, errors.New("cannot find argument for --month")
+		}
+
+		month, err := strconv.Atoi(args[idx+1])
+		if err != nil {
+			return Command{}, errors.New("argument for --month is not a number")
+		}
+
+		cmd.Month = month
+	}
+
+	return cmd, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/dmitriy-zverev/expense-tracker/cmd"
+)
+
+func main() {
+	cmd, err := cmd.ParseCommand(os.Args)
+	if err != nil {
+		fmt.Printf("\nError: %v\n", err)
+		return
+	}
+
+	fmt.Println(cmd)
 }
